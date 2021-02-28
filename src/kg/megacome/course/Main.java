751a034eddc2438433f388d5c1442e7e29856bc5
Alexander <tianalex999@gmail.com>
2021-02-28 16:37:31 +0600

    ff

--- src/kg/megacome/course/Main.java
+++ src/kg/megacome/course/Main.java
@@ -1,8 +1,7 @@
 package kg.megacome.course;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class Main {
 
@@ -131,7 +130,40 @@ public class Main {
             acerBrandsLinK.add(11_000_000, new Integer(bb));
         }
         System.out.println("Added 11_000_000 into LinkedList  and Latency is " + (System.currentTimeMillis()-PointCDF)+"ms");
+        System.out.println();
+        System.out.println("altering acerBrandsLinK (11 millions) which has random input integers into static Int MAX and MIN Value    ");
+        long regress= System.currentTimeMillis();
+        Collections.sort(acerBrandsLinK);
+        Collections.fill(acerBrandsLinK,Integer.MIN_VALUE );
+        System.out.println("Altering tables in LinkedList and fill int-Min Value, Latency is " + (System.currentTimeMillis()-regress)+"ms");
 
+        long  pushingUp= System.currentTimeMillis();
+        Collections.sort(asusBrand);
+Collections.fill(asusBrand, Integer.MAX_VALUE*150);
+        System.out.println("Altering tables in ArrayList and fill them Int-Max.val, Latency is " + (System.currentTimeMillis()-pushingUp)+"ms");
+        System.out.println();
+        System.out.println("Next Operation ");
+        long eperiment= System.currentTimeMillis();
+        Collections.sort (pokerCardArList);
+        Collections.fill(pokerCardArList,Integer.numberOfTrailingZeros(1500));
+        Collections.shuffle(pokerCardArList);
+        System.out.println("Do sorting  the  9_000_000 PokerCardArrayList  then fill it   by int.numberOfTrailingZeros and shuffle again"+ (System.currentTimeMillis()-eperiment)+"ms");
 
+long unusual= System.currentTimeMillis();
+Collections.sort(traditionalCard);
+        Collections.rotate(traditionalCard,50);
+        int bb= (int)Math.random()*(9852+1)+1;
+Collections.fill(traditionalCard,bb);
+        System.out.println("Do sorting  the  9_000_000 traditionalCard LinkList  then fill it   by random numbers ); then rotate them again" + (System.currentTimeMillis()-unusual)+"ms" );
+
+        System.out.println();
+        System.out.println("Last Test" );
+        long lastTest= System.currentTimeMillis();
+        int bbc= (int)Math.random()*(5985+1)+1;
+        Collections.sort(hpBrandLink);
+        Collections.fill(hpBrandLink,bbc);
+        Collections.shuffle(hpBrandLink);
+        Collections.nCopies(85,hpBrandLink);
+        System.out.println("Do sorting  the 7_000_000  then fill random numbers   then shaffle in the end  do .ncopies , and  latency  is  "+ (System.currentTimeMillis()-lastTest)+"ms" );
     }
 }
